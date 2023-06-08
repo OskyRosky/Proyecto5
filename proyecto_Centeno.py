48bcc0559f494f7dac3d569a96d468f2070f169e
DonMarco <marco500ag@gmail.com>
2023-06-08 16:30:42 -0600

    Df Marco
    Se agrega df Marco

--- proyecto_Centeno.py
+++ proyecto_Centeno.py
@@ -23,3 +23,12 @@ df = pd.DataFrame({
 
 # Print the DataFrame
 print(df)
+
+#Marco
+
+pelicula={"Nombre": ["Super Mario Bros", "The Whale", "Spiderman: Across", "Notting Hill", "Kiki:entregas a domicilio", "Green Book"], 
+"Género":["animada", "drama", "animada", "romance", "anime", "Drama" ],
+"Duración": ["1h32m", "1h57m", "2h20m", "2h04m", "1h42m", "2h10m"],
+"Puntaje Rotten Tomatoes": [60, 65, 96, 85, 99, 77]}
+pl= pd.DataFrame(pelicula)
+print(pl)
